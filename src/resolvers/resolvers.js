4d644f6a2d35bdcb44ad9036e21a0f81edef1a07
Oscar <newman.oscar@gmail.com>
2019-06-04 00:13:24 -0400

    arrow functions

--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -1,26 +1,20 @@
 // const { createUser } = require("auth/authentication")
 module.exports = {
-    resolvers: {
-        
-        Mutation: {
-            // signup args: { email, password, firstName, lastName, zipcode } per schema
-            signup: async (parent, args, context, info) => {
-                return await context.models.Authentication.signupUser(args);
-            },
-            // requestPasswordChange args: { email } per schema
-            requestPasswordChange: async (parent, args, context, info) => {
-                return await context.models.Authentication.requestPasswordChange(args);
-            },
-            // validateFromCode args: { verificationCode } per schema
-            validateFromCode: async (parent, args, context, info) => {
-                return await context.models.Authentication.verifyFromCode(args);
-            },
-            refreshAccess: async (parent, args, context, info) => {
-                return {
-                    
-                }
-            }
+  resolvers: {
+    Mutation: {
+      // signup args: { email, password, firstName, lastName, zipcode } per schema
+      signup: async (parent, args, context, info) =>
+        await context.models.Authentication.signupUser(args),
 
-        }
-    }
+      // requestPasswordChange args: { email } per schema
+      requestPasswordChange: async (parent, args, context, info) =>
+        await context.models.Authentication.requestPasswordChange(args),
+
+      // validateFromCode args: { verificationCode } per schema
+      validateFromCode: async (parent, args, context, info) =>
+        await context.models.Authentication.verifyFromCode(args),
+
+      refreshAccess: async (parent, args, context, info) => {},
+    },
+  },
 }
